5d008d7a1be75705fb63ae1e64ff63418fa1ba2b
tesddev <tesleem.amuda@gmail.com>
2026-02-02 18:36:25 +0100

    add initial project file

---
+++ ShortVideoFeed/ShortVideoFeed/ShortVideoFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoFeedApp.swift
+//  ShortVideoFeed
+//
+//  Created by Tes on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShortVideoFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShortVideoFeed/ShortVideoFeedTests/ShortVideoFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShortVideoFeedTests.swift
+//  ShortVideoFeedTests
+//
+//  Created by Tes on 02/02/2026.
+//
+
+import Testing
+@testable import ShortVideoFeed
+
+struct ShortVideoFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
