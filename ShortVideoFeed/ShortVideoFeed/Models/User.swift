b5b97b3924ffbdf58c78ff87eb54df26a99b0f68
tesddev <tesleem.amuda@gmail.com>
2026-02-04 19:37:57 +0100

    add user model

--- ShortVideoFeed/ShortVideoFeed/Models/User.swift
+++ ShortVideoFeed/ShortVideoFeed/Models/User.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: - User Model
+struct User: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let url: String
+    
+    var username: String {
+        // Extract username from name or generate from ID
+        return "@" + name.lowercased().replacingOccurrences(of: " ", with: "_")
+    }
+    
+    var profileURL: URL? {
+        URL(string: url)
+    }
+}
