b85e496a44bb54b7407228dee95dd13fa505952e
tesddev <tesleem.amuda@gmail.com>
2026-02-04 19:37:48 +0100

    add PexelsResponse

--- ShortVideoFeed/ShortVideoFeed/Models/PexelsResponse.swift
+++ ShortVideoFeed/ShortVideoFeed/Models/PexelsResponse.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// MARK: - Pexels API Response
+struct PexelsResponse: Codable {
+    let page: Int?
+    let perPage: Int?
+    let videos: [Video]?
+    let totalResults: Int?
+    let nextPage: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case page, videos
+        case perPage = "per_page"
+        case totalResults = "total_results"
+        case nextPage = "next_page"
+    }
+}
